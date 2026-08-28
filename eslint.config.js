6dfdd286bae6824edbd5621d979040cb0b59d3e0
abikananda <abikananda@users.noreply.github.com>
2026-08-29 01:39:17 +0530

    Exclude tool config files from source linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,17 @@ const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 
 module.exports = tseslint.config(
+  {
+    ignores: [
+      'node_modules/**',
+      'playwright-report/**',
+      'test-results/**',
+      'eslint.config.js',
+      'prettier.config.js',
+    ],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    ignores: ['node_modules/**', 'playwright-report/**', 'test-results/**'],
-  },
   {
     rules: {
       'no-console': 'off',
