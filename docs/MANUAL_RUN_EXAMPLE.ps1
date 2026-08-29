261f7da44373a309c129d2c2025970902ee02200
abikananda <abikananda@users.noreply.github.com>
2026-08-29 09:09:43 +0530

    Use usernames in manual run example

--- docs/MANUAL_RUN_EXAMPLE.ps1
+++ docs/MANUAL_RUN_EXAMPLE.ps1
@@ -1,16 +1,16 @@
 # Authenticate lender A once
-$env:LENDER_ID='LENDER_A'
+$env:LENDER_USERNAME='abikananda'
 npm run auth
 
 # Authenticate lender B once
-$env:LENDER_ID='LENDER_B'
+$env:LENDER_USERNAME='seconduser'
 npm run auth
 
 # For parallel execution, open two PowerShell terminals.
 # Terminal A:
-$env:LENDER_ID='LENDER_A'
+$env:LENDER_USERNAME='abikananda'
 npm test
 
 # Terminal B:
-$env:LENDER_ID='LENDER_B'
+$env:LENDER_USERNAME='seconduser'
 npm test
