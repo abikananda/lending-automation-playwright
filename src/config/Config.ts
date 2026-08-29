4c31c37f449da7e1b4b78db3f3267299efae4d76
abikananda <abikananda@users.noreply.github.com>
2026-08-29 08:44:35 +0530

    Require lender id for each run

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -28,6 +28,7 @@ function optional(name: string): string | undefined {
 export const config = {
   lendenClubUrl: required('LENDENCLUB_URL', 'https://app.lendenclub.com'),
   backendUrl: required('BACKEND_URL', 'http://localhost:8080'),
+  lenderId: required('LENDER_ID'),
   backendApiKey: optional('BACKEND_API_KEY'),
   backendAuthHeader: required('BACKEND_AUTH_HEADER', 'X-API-Key'),
   headless: boolEnv('HEADLESS', false),
